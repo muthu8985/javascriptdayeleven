a84af616e08b3afd096d277134199db954317b33
Muthu Kumar <muthukumarv8985@gmail.com>
2024-10-21 21:17:27 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-<p>Date.parse() returns the number of milliseconds between the date and January 1, 1970:
-
-</p>
+<p>
+  Date.parse() returns the number of milliseconds between the date and January
+  1, 1970:
+</p>;
 const msec = Date.parse("March 15, 2019");
 document.write(msec);
 const dapar = new Date(msec);
